7669d6f87d6011c81fcb971ca09b84830b76a6d0
D_h <54danghao@gmail.com>
2021-05-01 08:43:05 +0800

    接收日志并分发到不同Topic

---
+++ logger/src/main/java/log/maker/logger/controller/LoggerController.java
@@ -0,0 +1,35 @@
+package log.maker.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 接收生成的模拟日志并处理
+ */
+@RestController
+@Slf4j
+public class LoggerController {
+		@Autowired
+		KafkaTemplate kafkaTemplate;
+
+	public String appLog(@RequestBody String mockLog) {
+		log.info(mockLog);
+
+		// 根据日志类型发送到不同topic中
+		JSONObject jsonObject = JSON.parseObject(mockLog);
+		String startJson = jsonObject.getString("start");
+
+		if (startJson != null) {
+			kafkaTemplate.send("start", mockLog);
+		} else {
+			kafkaTemplate.send("event", mockLog);
+		}
+
+		return "success;";
+	}
+}
