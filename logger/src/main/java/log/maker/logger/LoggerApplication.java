3913275c6f4663718c99cca8550b884fb0a75b7b
D_h <54danghao@gmail.com>
2021-06-06 08:34:57 +0800

    发送模拟数据到指定Topic

--- logger/src/main/java/log/maker/logger/LoggerApplication.java
+++ logger/src/main/java/log/maker/logger/LoggerApplication.java
@@ -16,18 +16,17 @@ public class LoggerApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(LoggerApplication.class, args);
 		KafkaSender sender = run.getBean(KafkaSender.class);
+		int n = 10;
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < n; i++) {
 			sender.sendMsg("msg");
 			sender.sendCityInfo("city");
-
 			try {
 				Thread.sleep((new Random().nextInt(1000) + 200));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
 }
